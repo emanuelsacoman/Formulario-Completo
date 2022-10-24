0398a66d1cbc41e27a5486e2ac531b67e92f0546
EvS444 <emanuelsacoman@gmail.com>
2022-10-24 16:50:40 -0300

    Formulario Completo (até então sem bugs)

---
+++ script.js
@@ -0,0 +1,59 @@
+function enviar() {
+    let nom = document.getElementById('nom').value;
+    let tel = document.getElementById('tel').value;
+    let sex = document.getElementById('sex').value;
+    let ema = document.getElementById('ema').value;
+    let cpf = document.getElementById('cpf').value;
+    let est = document.getElementById('est').value;
+    let cid = document.getElementById('cid').value;
+    let rua = document.getElementById('rua').value;
+    let bai = document.getElementById('bai').value;
+    let cep = document.getElementById('cep').value;
+
+//  Código para fazer a data ficar com o padrão brasileiro
+
+    let dat = document.getElementById('dat').value;
+    let dataa = dat;
+    let datb = dataa.split('-').reverse().join('/');
+
+// Código para descobrir a idade do usuário
+
+var dataAtual = new Date();
+    
+    var data_nascimento = new Date(document.getElementById('dat').value);
+
+    var anos = dataAtual.getFullYear() - data_nascimento.getFullYear();
+
+    if(dataAtual.getMonth() != data_nascimento.getMonth())
+    {
+        if(dataAtual.getMonth() < data_nascimento.getMonth())
+        {
+            anos--;
+        }
+    }
+    else
+    {
+        if(dataAtual.getDate() < data_nascimento.getDate())
+        {
+            anos--;
+        }
+    }
+
+
+//  Alertas que aparecerão na tela após a finalização do formulário
+    
+     alert("Nome: "+nom + "\n"
+      + "Telefone: "+tel +"\n"
+       + "Sexo: "+sex + "\n"
+       + "Data de Nascimento: "+datb + "\n"
+       + "Email: "+ema + "\n"
+       + "CPF: "+cpf + "\n"
+       + "Estado: "+est + "\n"
+       + "Cidade: "+cid + "\n"
+       + "Rua: "+rua + "\n"
+       + "Bairro: "+bai + "\n"
+       + "Cep: "+cep);
+
+      alert("Sua idade: "+anos);
+
+}
